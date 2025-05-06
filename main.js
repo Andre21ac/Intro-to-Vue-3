dcf71d6da282d5776e7c298a060df07b282c682f
Andre21ac <andrecristovao15@gmail.com>
2025-05-06 11:15:20 -0300

    desafio concluído

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
+        },
+        removeFromCart() {
+            if(this.cart.length === 0) {
+                alert('o carrinho está vazio')
+            }
+            this.cart.pop()
+        }
+    }
 })
