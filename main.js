f6bbf817bc4e5aaf8f872e6d5ce21e2b438cd56d
Andre21ac <andrecristovao15@gmail.com>
2025-05-06 09:33:06 -0300

    Computed Properties

--- main.js
+++ main.js
@@ -4,21 +4,41 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-            ]
+            ],
+            selectedVariant: 0,
+            onSale: true
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            return this.selectedVariant = index
         }
+    },
+    computed: {
+        title() {
+            if(this.onSale === true) {
+                return this.brand + ' ' + this.product + ' ' + 'is on sale!'
+            }
+
+            else {
+                return this.brand + ' ' + this.product
+            }
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        // checkOnSale() {
+        //     return this.brand + ' ' + this.product + ' ' + 'is on sale!'
+        // }
     }
 })
