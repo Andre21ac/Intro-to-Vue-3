9a4d648eae67202c43da69e7393476e572b2c9f9
André Cristovão <145277659+Andre21ac@users.noreply.github.com>
2025-05-06 10:30:58 -0300

    Desafio concluído

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            cart: 0,
+            premium: false
         }
-    }
+    },
+    methods: {}
 })
