3ad4063f6fe4c55c56db269a1e4d9632d1fc01ba
Andre21ac <andrecristovao15@gmail.com>
2025-05-07 08:56:12 -0300

    Curso finalizado

---
+++ components/ReviewList.js
@@ -0,0 +1,22 @@
+app.component('review-list', {
+    props: {
+        reviews: {
+            type: Array,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    `<div class="review-container">
+        <h3>Reviews:</h3>
+        <ul>
+            <li v-for="(review, index) in reviews" :keys="index">
+                {{ review.name }} gave this {{ review.rating }} stars
+                <br/>
+                "{{  review.review }}"
+                <br/>
+                {{ review.name }} {{ review.recommend }} recommend
+            </li>
+        </ul>
+    </div>`
+})
